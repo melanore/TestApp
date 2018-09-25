c2ece491c8fc1d12a02d572a11a6ac5f962b56e1
melanore <max@socialcee.com>
2018-09-25 16:27:08 +0200

    initial

---
+++ TestApp.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApp.Data
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TestApp.Web/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace TestApp.Web
+{
+    public static class Program
+    {
+        public static void Main(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build().Run();
+    }
+}
